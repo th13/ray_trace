776dea52844110044542b7d0cff61bbedc5ae1e7
Trevor Helms <thelms@apple.com>
2025-04-09 17:43:25 -0700

    Render to raylib texture

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,7 @@
 const std = @import("std");
 const rt = @import("rt.zig");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer std.debug.assert(gpa.deinit() == .ok);
-    const allocator = gpa.allocator();
-
+fn render_scene(allocator: std.mem.Allocator) !rt.Image {
     var prng = std.Random.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.posix.getrandom(std.mem.asBytes(&seed));
@@ -56,7 +52,56 @@ pub fn main() !void {
         .color = rt.Color.rgb(130, 135, 125),
     });
 
-    const proj = try scene.project(allocator);
-    defer allocator.free(proj.data);
-    try proj.printP6();
+    try scene.spheres.append(rt.Sphere{
+        .name = "Christina's Sphere",
+        .center = .{ -220, -100, 300 },
+        .r = 90,
+        .color = rt.Color.rgb(180, 140, 200),
+    });
+
+    return try scene.project(allocator);
+}
+
+const rl = @import("raylib");
+fn show_rl(allocator: std.mem.Allocator, rendering: *const rt.Image) !void {
+    rl.initWindow(@intCast(rendering.width), @intCast(rendering.height), "Hello Raylib");
+    defer rl.closeWindow();
+
+    const pixels: []rl.Color = try allocator.alloc(rl.Color, rendering.width * rendering.height);
+    defer allocator.free(pixels);
+
+    for (pixels, 0..) |*p, i| p.* = rl.Color{ .r = rendering.data[i].r(), .g = rendering.data[i].g(), .b = rendering.data[i].b(), .a = 255 };
+
+    const image = rl.Image{
+        .data = pixels.ptr,
+        .width = @intCast(rendering.width),
+        .height = @intCast(rendering.height),
+        .format = .uncompressed_r8g8b8a8,
+        .mipmaps = 1,
+    };
+
+    const texture = try rl.loadTextureFromImage(image);
+    defer rl.unloadTexture(texture);
+
+    rl.setTargetFPS(1);
+
+    while (!rl.windowShouldClose()) {
+        rl.updateTexture(texture, pixels.ptr);
+
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(.white);
+        rl.drawTexture(texture, 0, 0, .white);
+    }
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    const rendered_img = try render_scene(allocator);
+    defer allocator.free(rendered_img.data);
+    try show_rl(allocator, &rendered_img);
 }
